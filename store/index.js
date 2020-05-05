a82853577179a34a27c0fe532b50dbe0b63cb09a
Mike Goldberg <mkegoldberg@yahoo.com>
2020-05-05 00:24:39 -0700

    reorder

--- store/index.js
+++ store/index.js
@@ -9,14 +9,14 @@ export const state = () => ({
             techUsed: ['Vue.js', 'Nuxt', 'Axios', 'JavaScript', 'API', 'Tailwindcss', 'REST'],
             siteLink: ''
         },
-        MessagePlatform: {
-            path: '/projects/message-platform/',
-            thumbnailPath: '/message_platform_thumb.png',
-            images: ['/message_list.png', '/message_detail.png'],
-            title: 'Internal Messaging Platform',
-            description: '<p class="pb-4">Using a RESTful api that our backend team built, I was able to design and develop a messaging system for our network of travel agents and admins to securely communicate internally through our B2B software. I took this project from concept to creation. Thanks to the ability to write multi-layered actions in the Vuex Store, users have the ability to scan through a preview list of their messages, mark as read or unread, mark a task as complete, or drill into a single message view to get the full details. Users are able to toggle between mailboxes using Nuxt\'s dynamic routing and async fetch calls.</p>',
-            techUsed: ['Vue.js', 'Nuxt', 'Axios', 'JavaScript', 'API', 'Tailwindcss', 'REST', 'Routing'],
-            siteLink: ''
+        AvoyaDashboard: {
+          path: '/projects/avoya-dashboard/',
+          thumbnailPath: '/avoya_dashboard_thumb.png',
+          images: ['/avoya_dashboard_top.png', '/avoya_dashboard_metric.png', '/avoya_dashboard_bot.png', '/avoya_dashboard_modal.png'],
+          title: 'B2B Dashboard',
+          description: '<p class="pb-4">Agent Power&trade;, Ayoya\'s proprietary B2B software, is a lead management and booking software for our network of Travel Agents to manage their customers. The dashboard is a overview of their current activities, sales reports, and performance metrics. It is a snapshot of the most crucial data a travel agent needs to have at any given moment. Almost every component relies on dynamic data securely being returned to have the most up to date information. Agency owners also have the option to change the view to see their employee\'s performance and status.</p>',
+          techUsed: ['Vue.js', 'Nuxt', 'Chart.js', 'Axios', 'JavaScript', 'API', 'Tailwindcss', 'REST'],
+          siteLink: ''
         },
         // LeadWidget: {
         //     path: '/projects/lead-widget/',
@@ -36,15 +36,6 @@ export const state = () => ({
             techUsed: ['Vue.js', 'Nuxt', 'Axios', 'JavaScript', 'API', 'Tailwindcss'],
             siteLink: ''
         },
-        AvoyaDashboard: {
-            path: '/projects/avoya-dashboard/',
-            thumbnailPath: '/avoya_dashboard_thumb.png',
-          images: ['/avoya_dashboard_top.png', '/avoya_dashboard_metric.png', '/avoya_dashboard_bot.png', '/avoya_dashboard_modal.png'],
-            title: 'B2B Dashboard',
-            description: '<p class="pb-4">Agent Power&trade;, Ayoya\'s proprietary B2B software, is a lead management and booking software for our network of Travel Agents to manage their customers. The dashboard is a overview of their current activities, sales reports, and performance metrics. It is a snapshot of the most crucial data a travel agent needs to have at any given moment. Almost every component relies on dynamic data securely being returned to have the most up to date information. Agency owners also have the option to change the view to see their employee\'s performance and status.</p>',
-            techUsed: ['Vue.js', 'Nuxt', 'Chart.js', 'Axios', 'JavaScript', 'API', 'Tailwindcss', 'REST'],
-            siteLink: ''
-        },
         AvoyaMembership: {
             path: '/projects/avoya-membership/',
             thumbnailPath: '/avoya_membership_thumb.png',
@@ -53,6 +44,15 @@ export const state = () => ({
             description: '<p class="pb-4">As my introductory project to Vue.js, this project was a terrific experience of how great a framework it is. The membership platform is actually a hybrid that uses coldfusion to handle the api end points in the parent pages, then utilizes the "Props" to pass the data to the vue components</p><p class="pb-4">The membership platform is a system in which users can store their personal information, save their favorite trips, and write reviews<p>',
             techUsed: ['Vue.js', 'JavaScript', 'jQuery', 'Ajax'],
             siteLink: ''
+        },
+        MessagePlatform: {
+          path: '/projects/message-platform/',
+          thumbnailPath: '/message_platform_thumb.png',
+          images: ['/message_list.png', '/message_detail.png'],
+          title: 'Internal Messaging Platform',
+          description: '<p class="pb-4">Using a RESTful api that our backend team built, I was able to design and develop a messaging system for our network of travel agents and admins to securely communicate internally through our B2B software. I took this project from concept to creation. Thanks to the ability to write multi-layered actions in the Vuex Store, users have the ability to scan through a preview list of their messages, mark as read or unread, mark a task as complete, or drill into a single message view to get the full details. Users are able to toggle between mailboxes using Nuxt\'s dynamic routing and async fetch calls.</p>',
+          techUsed: ['Vue.js', 'Nuxt', 'Axios', 'JavaScript', 'API', 'Tailwindcss', 'REST', 'Routing'],
+          siteLink: ''
         },
         SilverDragon: {
             path: '/projects/silver-dragon/',
